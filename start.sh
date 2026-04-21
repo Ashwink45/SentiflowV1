3bf33c08eff5477f47c719105870b2cf969a8cbb
Ashwin <ashk99@users.noreply.huggingface.co>
2026-04-21 06:30:04 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 # (Replace 'engine' with your FastAPI filename, e.g., 'main' or 'analyze')
 uvicorn main:app --host 0.0.0.0 --port 8000 &
 
-sleep 5
+sleep 50
 
 # 2. Start the Streamlit UI in the foreground
 streamlit run index.py --server.port 7860 --server.address 0.0.0.0 --server.enableCORS false --server.enableXsrfProtection false
